e80f03d3f30fcdb578efe01009afed1ec9989a8d
Diego-spes <A01708017@tec.mx>
2024-09-12 08:41:05 -0500

    Nuevos estilos CSS y modificaciones al page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,18 @@ export default function HomePage() {
   
   return (
     <div>
+      <h1 className="container">
+        Bienvenido a nuestra web de semana Tec
+      </h1>
+      <div className="container">
+        <h3>Equipo 8:</h3>
+      <ul>
+  <li className="container">Jorge Emiliano Pomar Mendoza A01709338  <ul> <li> @pomaremiliano</li></ul> </li>
+  <li className="container">Ivan Ricardo Paredes Avilez A01705083  <ul> <li> @IvanParedesA</li></ul> </li>
+  <li className="container">Diego Reséndiz Fernández A01708017 <ul> <li> @Diego-spes</li></ul> </li>
+  <li className="container">Francisco Couttolenc Ortiz A01754959  <ul> <li>@Couttolenc01</li></ul> </li>
+</ul> </div>
+      
       <FetchPokemones />
       <FetchPerros />
     </div>
